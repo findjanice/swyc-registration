93fe9e6b0f396a228a714e43cee2be1b45737bec
Janice <findjanice@gmail.com>
2015-08-23 15:46:38 -0600

    added css scrolling effect

--- public/about/aboutCtrl.js
+++ public/about/aboutCtrl.js
@@ -1,9 +1,17 @@
-app.controller('aboutCtrl', function($scope, $routeParams, $route, $location) {
+app.controller('aboutCtrl', function($scope, $routeParams, $route, $location,
+  dataService) {
   $scope.$watch(function() {
     return $route.current.css;
   }, function(value) {
     $scope.css = value;
   });
+
+  $scope.aboutSWYC = function() {
+    $scope.aboutInfo = dataService.aboutSWYC();
+  };
+
+  $scope.aboutSWYC();
+
 
   $scope.test = "HEllo World";
   //end homeCtrl
--- public/services/dataService.js
+++ public/services/dataService.js
@@ -1,21 +1,31 @@
-var aboutSWYC = [
-  "GYC Southwest is a grassroots movement of Seventh-day Adventist youth and young adults.  Our purpose is to support the mission and message of the Seventh-day Adventist Church by encouraging young people in the southwestern United States to be actively involved in the ministry of the Church and equipping them for service to God and man.",
-];
+app.service('dataService', function($http, $q) {
 
-var purpose = [
-  "The purpose of GYC Southwest is to support the mission and message of the Seventh-day Adventist Church by encouraging young people in the southwestern United States to be actively involved in the ministry of the Church. It seeks to do this by:",
-  "Providing an annual meeting, the Southwest Youth Conference (SWYC), to revitalize our love for His Word, renew our commitment to work where He leads, and deepen our desire to see Him return",
-  "Encouraging a full dedication to Christ through earnest study of the Bible, prayer and service to others ",
-  "Meeting each year for worship and Bible study",
-  "Collaborating with local Seventh - day Adventist churches and organizations",
-  "Reaching out to the young people in the southwestern United States,
-  encouraging them to an acceptance and full commitment to Christ ",
-  "Using outreach to touch our local and global community in tangible ways",
-  "and Hastening the soon return of Jesus Christ through proclamation of the Three Angels’ Messages, thus encouraging others to prepare to meet Him."
-];
+  var aboutSWYC = [{
+    about: "GYC Southwest is a grassroots movement of Seventh-day Adventist youth and young adults.  Our purpose is to support the mission and message of the Seventh-day Adventist Church by encouraging young people in the southwestern United States to be actively involved in the ministry of the Church and equipping them for service to God and man."
+  }]
 
-var conferenceInfo = {
-  theme: "Christ Our Righteousness",
-  date: "September 4 - 7, 2015"
-  location: "Pine Springs Ranch in Mountain, CA"
-}
+  // var purpose = [
+  //   "The purpose of GYC Southwest is to support the mission and message of the Seventh-day Adventist Church by encouraging young people in the southwestern United States to be actively involved in the ministry of the Church. It seeks to do this by:",
+  //   "Providing an annual meeting, the Southwest Youth Conference (SWYC), to revitalize our love for His Word, renew our commitment to work where He leads, and deepen our desire to see Him return",
+  //   "Encouraging a full dedication to Christ through earnest study of the Bible, prayer and service to others ",
+  //   "Meeting each year for worship and Bible study",
+  //   "Collaborating with local Seventh - day Adventist churches and organizations",
+  //   "Reaching out to the young people in the southwestern United States,
+  //   encouraging them to an acceptance and full commitment to Christ ",
+  //   "Using outreach to touch our local and global community in tangible ways",
+  //   "and Hastening the soon return of Jesus Christ through proclamation of the Three Angels’ Messages, thus encouraging others to prepare to meet Him."
+  // ];
+
+  // var conferenceInfo = {
+  //   theme: "Christ Our Righteousness",
+  //   date: "September 4 - 7, 2015"
+  //   location: "Pine Springs Ranch in Mountain, CA"
+  // }
+
+  this.aboutSWYC = function() {
+    return aboutSWYC;
+  };
+
+  //end service
+
+})
