39e84e2210dbde6038bc2424c07cdec408a10711
Janice <findjanice@gmail.com>
2015-10-01 07:14:36 -0600

    cleaned up code

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -4,19 +4,15 @@ var newUserId;
 
 module.exports = {
   create: function(req, res) {
-    console.log('this is userId', req.body);
     var newUser = new User(req.body);
     newUser.save(function(err, result) {
       newUserId = result._id;
-      console.log(newUserId);
       if (err) return res.status(500).send(err);
       res.send(result)
-      console.log('this is send result', result);
     });
   },
   read: function(req, res) {
     User.find(req.query).exec(function(err, result) {
-      console.log('this is read result', result);
       if (err) return res.status(500).send(err);
       res.send(result);
     });
